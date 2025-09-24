27afca25aa5f3a9735f286df6cc03f9beb961dcc
vr700 <104796510+vr700@users.noreply.github.com>
2025-09-23 23:54:25 -0500

    chore(vite): set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
+  base: "/HI-Plate-Web/",
+
   server: {
     host: "::",
     port: 8080,
@@ -12,4 +14,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  plugins: [react()], // 👈 ¡No olvides los plugins!
 }));
